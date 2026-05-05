1578da069a49331a5f26554c6a48bbf9cc4ca8d0
Gahan-Das <1234gahandas@gmail.com>
2026-05-05 15:46:00 +0530

    Solution using Python

---
+++ 13.RomanToInteger/Roman_To_Integer.py
@@ -0,0 +1,77 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        limit = len(s)
+        i = 0
+        while i < limit:
+            if s[i] == 'I':
+                try:
+                    if s[i+1] == 'V':
+                        ans += 4
+                        i += 2
+                    elif s[i+1] == 'X':
+                        ans += 9
+                        i += 2
+                    else:
+                        ans += 1
+                        i += 1
+                    if i == limit:
+                        break
+                except:
+                    ans += 1
+                    break
+            if s[i] == 'X':
+                try:
+                    if s[i+1] == 'L':
+                        ans += 40
+                        i += 2
+                    elif s[i+1] == 'C':
+                        ans += 90
+                        i += 2
+                    else:
+                        ans += 10
+                        i += 1
+                    if i == limit:
+                        break
+                except:
+                    ans += 10
+                    break
+            if s[i] == 'C':
+                try:
+                    if s[i+1] == 'D':
+                        ans += 400
+                        i += 2
+                    elif s[i+1] == 'M':
+                        ans += 900
+                        i += 2
+                    else:
+                        ans += 100
+                        i += 1
+                    if i == limit:
+                        break
+                except:
+                    ans += 100
+                    break
+            if s[i] == 'V':
+                ans += 5
+                i += 1
+                if i == limit:
+                    break
+            if s[i] == 'L':
+                ans += 50
+                i += 1
+                if i == limit:
+                    break
+            if s[i] == 'D':
+                ans += 500
+                i += 1
+                if i == limit:
+                    break
+            if s[i] == 'M':
+                ans += 1000
+                i += 1
+        return ans

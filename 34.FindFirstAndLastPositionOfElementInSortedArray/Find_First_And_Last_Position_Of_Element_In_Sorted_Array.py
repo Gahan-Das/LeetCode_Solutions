5c1461247064d567f9aa1ca266a5ade509d1f020
Gahan-Das <1234gahandas@gmail.com>
2026-05-24 15:11:03 +0530

    Solved using Python

---
+++ 34.FindFirstAndLastPositionOfElementInSortedArray/Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        try:
+            return [nums.index(target), len(nums) - 1 - sorted(nums, reverse=True).index(target)]
+        except Exception as e:
+            return [-1, -1]
+        

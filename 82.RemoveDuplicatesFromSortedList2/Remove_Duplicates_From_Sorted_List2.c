0426a34d4730f7383d8585a6aad0a35705b5637e
Gahan-Das <1234gahandas@gmail.com>
2026-07-14 19:11:33 +0530

    Small change

--- 82.RemoveDuplicatesFromSortedList2/Remove_Duplicates_From_Sorted_List2.c
+++ 82.RemoveDuplicatesFromSortedList2/Remove_Duplicates_From_Sorted_List2.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
 struct ListNode* deleteDuplicates(struct ListNode* head) {
     if(head == NULL){
         return head;

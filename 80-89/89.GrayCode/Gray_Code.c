7ea3b10552f6abc693cbef71263527d18b4309ba
Gahan-Das <1234gahandas@gmail.com>
2026-07-26 15:28:29 +0530

    Updated existing code

--- 80-89/89.GrayCode/Gray_Code.c
+++ 80-89/89.GrayCode/Gray_Code.c
@@ -4,48 +4,34 @@
 int binInt(long int n){
     int num = 0, count = 0;
     while(n > 0){
-        num = (int)pow(2,count++)*(n%10) + num;
+        num = (long int)pow(2,count++)*(n%10) + num;
         n /= 10;
     }
-    // printf("\t%d", num);
     return num;
 }
 int binGray(long int n){
     long int num = 0, copy = n;
     int count = 0;
     while(copy > 0){
-        num = ((copy%100)/10 ^ copy%10)*(int)pow(10,count++) + num;
+        num = ((copy%100)/10 ^ copy%10)*(long int)pow(10,count++) + num;
         copy /= 10;
     }
-    // printf("\t%ld", num);
     return binInt(num);
 }
 int intBin(int n){
     long int num = 0;
     int x = n, count = 0;
     while(x > 0){
-        num = (x % 2)*(int)pow(10,count++) + num;
+        num = (x % 2)*(long int)pow(10,count++) + num;
         x = x/2;
     }
-    // printf("\n%ld",num);
     return binGray(num);
 }
 int* grayCode(int n, int* returnSize) {
-    int* answer = (int*)malloc((int)pow(2,n)*sizeof(int));
-    int ans[70000] = {0};
-    for(int i = 0; i < (int)pow(2,n); i++){
+    int* answer = (int*)malloc((long int)pow(2,n)*sizeof(int));
+    for(int i = 0; i < (long int)pow(2,n); i++){
         answer[i] = intBin(i);
     } 
-    // for(int i = 0; i < (int)pow(2,n); i++)
-    //     printf("%d\t", *(answer+i));
     *returnSize = (int)pow(2,n);
     return answer;
-}
-int main(){
-    int n = 10;
-    int size;
-    int* arr = grayCode(n, &size);
-    for(int i = 0; i < size; i++){
-        printf("%d\t", arr[i]);
-    }
 }

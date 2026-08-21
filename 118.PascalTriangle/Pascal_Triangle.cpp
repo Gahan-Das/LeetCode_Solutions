42a8858d1153c0bb1c6a0459b65bd3bdc8e2e287
Gahan-Das <1234gahandas@gmail.com>
2026-08-21 22:53:58 +0530

    Solved using C++

---
+++ 118.PascalTriangle/Pascal_Triangle.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i = 0; i < numRows; i++){
+            vector<int> temp;
+            if(i == 0){
+                temp.push_back(1);
+            }
+            else{
+                temp.push_back(1);
+                for(int j = 1; j < i; j++){
+                    temp.push_back(ans[i-1][j]+ans[i-1][j-1]);
+                }
+                temp.push_back(1);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
